f5743703815c99f8a1bf6ea131da347b33ab9d01
Sven Hesse <drmccoy@drmccoy.de>
2021-08-14 12:12:05 +0200

    Add CL check macro that queries for spellbook

---
+++ macros/roll_caster_level-ask.js
@@ -0,0 +1,37 @@
+const tokens = canvas.tokens.controlled;
+const caster = tokens[0];
+
+if (tokens.length !== 1) {
+	ui.notifications.warn("Please select a token");
+} else {
+	let actor = tokens[0].actor;
+	let spells = actor.data.data.attributes.spells;
+
+	if (spells.usedSpellbooks.length < 1) {
+		ui.notifications.warn("You don't cast.");
+	} else {
+		const buttons = {};
+
+		spells.usedSpellbooks.forEach((type) => {
+			let spellbook = getProperty(spells, `spellbooks.${type}`);
+
+			buttons[spellbook.name] = {
+				label: spellbook.name,
+				callback: () => {
+					rollCheck(actor, type);
+				},
+			}
+		});
+
+		new Dialog({
+			title: "Roll Caster Level Check!",
+			content: "<p>Choose a spellbook</p>",
+			buttons: buttons,
+		}).render(true);
+
+	}
+}
+
+function rollCheck(actor, spellbook) {
+	actor.rollCL(spellbook);
+}
